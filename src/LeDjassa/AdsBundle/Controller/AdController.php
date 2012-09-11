d6730f9da7f5f6e0e20f54a82554d9abb3b05434
alagbelandry <as.landry@gmail.com>
2012-09-11 22:12:15 +0200

    add method to get city and save objet instance of form

--- src/LeDjassa/AdsBundle/Controller/AdController.php
+++ src/LeDjassa/AdsBundle/Controller/AdController.php
@@ -5,8 +5,13 @@ namespace LeDjassa\AdsBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
 use LeDjassa\AdsBundle\Model\Ad;
+use LeDjassa\AdsBundle\Model\City;
+use LeDjassa\AdsBundle\Model\Quarter;
 use LeDjassa\AdsBundle\Form\Type\AdType;
+use LeDjassa\AdsBundle\Model\CityQuery;
+use LeDjassa\AdsBundle\Model\QuarterQuery;
 
 /**
  * @Route("/annonces")
@@ -19,15 +24,39 @@ class AdController extends Controller
 	*/
     public function addAction()
     {
-    	$request = $this->get('request');
+    	
         $ad = new Ad();
         $form = $this->createForm(new AdType(), $ad);
 
-        if ('POST' === $request->getMethod()) {
+        $request = $this->get('request');
+        if ('POST' === $request->getMethod()) { 
         	$form->bindRequest($request);
+
+            // get parameters list
+            $parametersList = $request->request->get('ad');
+
+             // get city
+            $idCity = isset($parametersList['city']['name']) ? $parametersList['city']['name'] : '';
+            $city = CityQuery::create()
+                ->findOneById($idCity);
+
+            // add city to ad
+            $ad->setCity($city);
+
+            // add city to quarter
+            $nameQuarter = isset($parametersList['city']['quarter']['name']) ? $parametersList['city']['quarter']['name'] : '';
+            if ($city instanceof City && !empty($nameQuarter)) {
+                $quarter = new Quarter();
+                $quarter->setName($nameQuarter)
+                        ->setCity($city);
+            }
 
         	if ($form->isValid()) {
         		$ad->save();
+
+                if($quarter instanceof  Quarter) {
+                    $quarter->save();
+                }
         	}
 
         	//return $this->redirect($this->generateUrl('ad_success'));
@@ -35,5 +64,30 @@ class AdController extends Controller
         
         return  array('form' => $form->createView(), 'ad' => $ad);
 		
+    }
+
+    /**
+    * Return city list depending area
+    * @param Area $area region
+    * @return Response ajax response
+    * @Route("/recupererVilles", name="city_from_area_get")
+    * 
+    */
+    public function getCityFromAreaAction()
+    {
+        $cityList = array();
+        $request = $this->get('request');
+        $areaId = $request->get('areaId');
+
+        if ($request->isXmlHttpRequest()) {
+            $cityList = CityQuery::create()
+                                ->filterByAreaId($areaId)
+                                ->find()
+                                ->toKeyValue("Id", "Name"); 
+        }
+ 
+        $response = new Response(json_encode($cityList));
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
     }
 }

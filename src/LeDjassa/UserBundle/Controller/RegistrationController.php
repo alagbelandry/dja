393de70d4a535c0fefaa5c4a9f48a8b576c49acc
alagbelandry <as.landry@gmail.com>
2012-10-07 16:21:16 +0200

    extending FOSUserBundle\registrationController

---
+++ src/LeDjassa/UserBundle/Controller/RegistrationController.php
@@ -0,0 +1,13 @@
+<?php
+namespace LeDjassa\UserBundle\Controller;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use FOS\UserBundle\Controller\RegistrationController as BaseController;
+
+class RegistrationController extends BaseController
+{
+    public function registerAction()
+    {
+        return parent::registerAction();
+    }
+}

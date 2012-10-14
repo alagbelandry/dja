a58549724970ebf6c102c20cbe2ed6b9733037d4
alagbelandry <as.landry@gmail.com>
2012-10-14 16:15:10 +0200

    add form for edit ad

---
+++ src/LeDjassa/AdsBundle/Form/Type/AdEditType.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace LeDjassa\AdsBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * Class for manage edit ad form
+ *
+ * @author Landry Alagbe
+ */
+class AdEditType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('description', 'textarea', array(
+            'label' => 'Description :',
+        ));
+
+        $builder->add('price', 'money', array(
+            'label'    => 'Prix :',
+            'currency' => 'CFA',
+            'required' => false, 
+        ));
+
+        // Picture ad relation 
+        $builder->add('picture_ads', 'collection', array('type'         => new PictureAdType(),
+                                                        'prototype'     => true,
+                                                        'allow_add'     => true,
+                                                        'allow_delete'  => true,
+                                                        'required'      => false,
+                                                        'by_reference'  => false,
+                                                        'label'         => 'Photos d\'illustration',
+
+        ));
+
+        $builder->add('user_password', 'password', array(
+            'label'         => 'Mot de passe :',
+            'property_path' => false,
+            'attr'          => array(
+                'autocomplete' => 'off'
+        )));
+    }
+   
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'LeDjassa\AdsBundle\Model\Ad',
+        ));
+    }
+
+    public function getName()
+    {
+        return 'ad_edit';
+    }
+} 

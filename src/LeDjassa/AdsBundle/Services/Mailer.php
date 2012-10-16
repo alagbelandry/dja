6a9baff3dafe55e9ad1d26a07be076efc8f885fa
alagbelandry <as.landry@gmail.com>
2012-10-17 01:15:24 +0200

    configure service for sending contact email

--- src/LeDjassa/AdsBundle/Services/Mailer.php
+++ src/LeDjassa/AdsBundle/Services/Mailer.php
@@ -5,6 +5,8 @@ namespace LeDjassa\AdsBundle\Services;
 use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
 use Symfony\Component\Routing\RouterInterface;
 use LeDjassa\AdsBundle\Model\Ad;
+use LeDjassa\AdsBundle\Model\InterestedUser;
+use LeDjassa\AdsBundle\Model\InformationUser;
 
 class Mailer
 {
@@ -38,7 +40,39 @@ class Mailer
             'urlDelete' 		 => $urlDelete,
         ));
 
-        $this->sendEmailMessage($rendered, $subject, "alagbe.landry@gmail.com", $ad->getUserEmail());
+        $this->sendEmailMessage($rendered, $subject, $this->parameters["email"]["noreply"], $ad->getUserEmail());
+    }
+
+    public function sendInterestedUserEmailMessage(Ad $ad, InterestedUser $interestedUser)
+    {
+        $subject = sprintf("Votre annonce \"%s\" interesse %s",
+            $ad->getTitle(),
+            $interestedUser->getName()
+        );
+
+        $template = $this->parameters['template']['interested_user.contact'];
+        $rendered = $this->templating->render($template, array(
+            'ad'       => $ad->getProperties(),
+            'message'  => $interestedUser->getMessage(),
+            'urlShow'  => $urlShow = $this->router->generate('ad_show', array('idAd' => $ad->getId()), true),
+            'name'     => $interestedUser->getName(),
+            'email'    => $interestedUser->getEmail(),
+            'phone'    => $interestedUser->getPhone(),
+        ));
+
+        $this->sendEmailMessage($rendered, $subject, $interestedUser->getEmail(), $ad->getUserEmail());
+    }
+
+    public function sendContactEmailMessage(InformationUser $informationUser)
+    {
+        $template = $this->parameters['template']['contact'];
+        $rendered = $this->templating->render($template, array(
+            'message'  => $informationUser->getMessage(),
+            'name'     => $informationUser->getName(),
+            'email'    => $informationUser->getEmail(),
+        ));
+
+        $this->sendEmailMessage($rendered, $informationUser->getSubject(), $informationUser->getEmail(), $this->parameters["email"]["contact"]);
     }
 
     protected function sendEmailMessage($body, $subject, $fromEmail, $toEmail)

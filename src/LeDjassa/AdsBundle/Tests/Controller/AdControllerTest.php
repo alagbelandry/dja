a2312854bbdae2c1768405c766592cc8129cccd7
alagbelandry <as.landry@gmail.com>
2012-10-18 01:06:02 +0200

    improve test

--- src/LeDjassa/AdsBundle/Tests/Controller/AdControllerTest.php
+++ src/LeDjassa/AdsBundle/Tests/Controller/AdControllerTest.php
@@ -3,31 +3,33 @@
 namespace LeDjassa\AdsBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class AdControllerTest extends WebTestCase
 {
-	public function testIndexAction()
+
+	public function testIndex()
 	{
 		$client = static::createClient();
 
-		$crawlerListAd = $client->request('GET', 'http://dev.ledjassa.com/app_dev.php');
-		var_dump($crawlerListAd);die;
+		$crawlerListAd = $client->request('GET', '/annonces/');
+
 		// page list ad
 		$this->assertEquals(1, $crawlerListAd->filter('h3:contains("Toutes les annonces")')->count());
 
 		// at least one ad
-		$this->assertGreaterThan(0, $crawlerListAd->filter('<ul>')->count());
-
+		$this->assertTrue($crawlerListAd->filter('ul')->count() > 0);
+		
 		// link show ad
-		$linkShowAd = $crawlerListAd->selectLink('Consulter l\'annonce')->first()->link();
-		$crawlerShowAd = $linkShowAd->click($linkShowAd);
+		$linkShowAd = $crawlerListAd->selectLink("show_ad")->first()->link();
 
+		$crawlerShowAd = $linkShowAd->click($linkShowAd);
 		$crawlerShowAd = $client->followRedirect();
 
 		$this->assertEquals(1, $crawlerShowAd->filter('body:contains("Details de l\'annonce")')->count());
 	}
 
-	public function testAddAction()
+	public function testAdd()
 	{
 		$client = static::createClient();
 

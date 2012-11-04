264ed8bf34bfc920e9816504e008abebfc4a7974
alagbelandry <as.landry@gmail.com>
2012-11-04 20:05:00 +0100

    add utils method

--- src/LeDjassa/AdsBundle/Model/AdQuery.php
+++ src/LeDjassa/AdsBundle/Model/AdQuery.php
@@ -3,7 +3,45 @@
 namespace LeDjassa\AdsBundle\Model;
 
 use LeDjassa\AdsBundle\Model\om\BaseAdQuery;
+use \Criteria;
 
 class AdQuery extends BaseAdQuery
-{
+{   
+    /**
+     * Search ad by category, area and (title or description)
+     * @param string $category category search
+     * @param string $area area search
+     * @param string $title title or description search
+     * @return criteria
+     */
+	public function searchByCategoryAndAreaAndTitleOrDescription($category, $area, $title)
+	{
+        if ($category instanceof Category) {
+            $this->filterByCategory($category);
+        }
+
+        if ($area instanceof Area) {
+            $cities = $area
+                ->getCities()
+                ->toKeyValue("Id");
+
+            $this->filterByCityId(array_keys($cities));
+        }
+     
+        if (!empty($title)) {                
+            $this
+                ->condition('filterByTitle', 'ad.Title LIKE ?', $title) 
+                ->condition('filterByDescription', 'ad.Description LIKE ?', $title)
+                ->combine(array('filterByTitle', 'filterByDescription'), Criteria::LOGICAL_OR);
+        }
+
+        return $this;
+	}
+
+    /**
+     * Filter by ad live (available)
+     */
+    public function filterByLive() {
+        return $this->filterByStatut(Ad::STATUT_CREATED);
+    }
 }

1329b714801692f680683296d4cee9d89145de10
alagbelandry <as.landry@gmail.com>
2012-12-19 04:56:54 +0100

    add conf for capifony

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,14 +1,11 @@
 set :application, "auportdadjame"
 set :domain,      "auportdadjame.com"
 set :deploy_to,   "/var/www/auportdadjame.com"
-set :app_path,    "app"
 
 set :repository,  "git@github.com:alagbelandry/dja.git"
 set :scm,         :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
 
 set :model_manager, "propel"
-# Or: `propel`
 
 role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain                         # This may be the same as your `Web` server
@@ -20,6 +17,8 @@ set :shared_files,      ["app/config/parameters.ini"]
 set :shared_children,   [app_path + "/logs", web_path + "/uploads", "vendor"]
 set :update_vendors, true
 set :use_composer, true
+
+set :user, "as67376-ovh"
 
 # Be more verbose by uncommenting the following line
 # logger.level = Logger::MAX_LEVEL

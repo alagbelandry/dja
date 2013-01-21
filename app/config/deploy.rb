831efb33227244d0323ae9a15ce0b087ba2d74ca
alagbelandry <as.landry@gmail.com>
2013-01-22 00:48:54 +0100

    up conf

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -9,6 +9,7 @@ set :repository,  "git@ssh.github.com:alagbelandry/dja.git"
 set :scm,         :git
 
 set :model_manager, "propel"
+set :deploy_via, :copy
 
 role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain                         # This may be the same as your `Web` server
@@ -19,16 +20,14 @@ set  :keep_releases,  3
 set :php_bin, "/usr/local/bin/php.ORIG.5_4 -c /usr/local/lib/php.ini-2 -v"
 
 set :shared_files,      ["app/config/parameters.ini"]
-set :shared_children,   [app_path + "/logs", web_path + "/uploads", "vendor"]
+set :shared_children,   [app_path + "/logs", web_path + "/uploads"]
+set :vendors_mode, "install"
 set :use_composer, true
+set :composer_options,  "--verbose --prefer-dist"
 set :dump_assetic_assets, true
-set :update_vendors, true
 
 set :user, "auportda"
 set :use_sudo, false
-
-
-set :deploy_via, :copy
 
 # Use copy to bypass firewall...
 set :copy_strategy, :export
@@ -38,6 +37,27 @@ set :copy_compression, :gzip
 
 set :symfony_env_prod, "staging"
 set :writable_dirs,     ["app/cache", "app/logs"]
+
+# Symfony2 2.1
+before 'symfony:composer:update', 'symfony:copy_vendors'
+
+namespace :symfony do
+  desc "Copy vendors from previous release"
+  task :copy_vendors, :except => { :no_release => true } do
+    if Capistrano::CLI.ui.agree("Do you want to copy last release vendor dir then do composer install ?: (y/N)")
+      capifony_pretty_print "--> Copying vendors from previous release"
+
+      run "cp -a #{previous_release}/vendor #{latest_release}/"
+      capifony_puts_ok
+    end
+  end
+end
+
+case vendors_mode
+  when "upgrade" then symfony.vendors.upgrade
+  when "install" then symfony.vendors.install
+  when "reinstall" then symfony.vendors.reinstall
+end
 
 # Be more verbose by uncommenting the following line
 logger.level = Logger::MAX_LEVEL
